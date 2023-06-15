508996a9af357f1addd2c9fc4b1e01bc42ee2ca2
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-06-15 20:45:04 +0530

    base: Introduce Smart 5G service [1/3]
    Current functionality:
    - Disable 5G in battery saver mode
    - Disable 5G when not on mobile data (such as wifi)
    - Disable 5G on non-active data sim
    
    Not smart enough yet, but we'll get there.
    
    Change-Id: Iac71c1de51429e2287162bb9e45ff4aba3bd0ac9
    Signed-off-by: Daniel Jacob Chittoor <djchittoor47@gmail.com>
    
    Smart5gService: Avoid early return with multiple subs
    
    When using dual sim with second sim as the active data sub, there can
    be a case where update() returns at first sub id which breaks smart 5G
    functionality. Use continue instead of return as intended.
    
    Change-Id: I4d70a2fcf3ec816a5f964c3a736841f33f6ca982
    
    Change-Id: Iac71c1de51429e2287162bb9e45ff4aba3bd0ac9
    Signed-off-by: Daniel Jacob Chittoor <djchittoor47@gmail.com>
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6781,6 +6781,12 @@ public final class Settings {
         public static final String SCREENSHOT_SHUTTER_SOUND = "screenshot_shutter_sound";
 
         /**
+         * Whether to enable smart 5G mode
+         * @hide
+         */
+        public static final String SMART_5G = "smart_5g";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
